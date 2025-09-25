40000666e0d9ce734a3355558cf28788b6decf9c
Zeze-sye <cgods340@gmail.com>
2025-09-25 20:57:15 +0100

    Add files via upload

---
+++ ThursdayAssignment.java
@@ -0,0 +1,99 @@
+import java.util.Scanner;
+public class ThursdayAssignment {
+    public static void main(String[] args){
+        // reversed number
+
+        Scanner sc = new Scanner(System.in);
+
+//        int num;
+//        int reversedNum = 0;
+//
+//        System.out.print("Enter any number: ");
+//        num = sc.nextInt();
+//
+//        while (num > 0){
+//            int digit = num % 10;
+//            reversedNum = reversedNum*10 + digit;
+//            num = num /10;
+//        }
+//        System.out.printf("The reversed number is %d", reversedNum);
+
+        // prime number
+//        int num_one;
+//        int count = 0;
+//        System.out.print("Enter a number: ");
+//        num_one = sc.nextInt();
+//
+//        if(num_one < 1){
+//            System.out.printf("%d is not a prime number",num_one);
+//        }
+//        else{
+//            for(int i = 2; i < num_one; i++){
+//                if(num_one % i == 0){
+//                    count ++;
+//                    break;
+//
+//                }
+//            }
+//            if(count == 0){
+//                System.out.printf("%d is a prime number",num_one);
+//            }
+//            else{
+//                System.out.printf("%d is not a prime number",num_one);
+//            }
+//        }
+
+        // Vowel count
+//        String strings;
+//        int count_two = 0;
+//        String vowels = "aeiouUOIEA";
+//
+//
+//        System.out.print("Enter any string: ");
+//        strings = sc.nextLine();
+//
+//        for(int i =0; i< strings.length(); i++){
+//            char character = strings.charAt(i);
+//            if(vowels.indexOf(character) != -1){  // if(String.valueOf(ch). matches("[aeiouAEIOU]")
+//                count_two ++;
+//            }
+//
+//        }
+//        System.out.printf("This string contains %d vowels",count_two);
+
+        // Factorial
+//        int num_three;
+//        long answer = 1;
+//
+//        System.out.print("Enter a number: ");
+//        num_three = sc.nextInt();
+//
+//        for(int i = num_three; i >0;i-- ){
+//            answer *= i;
+//        }
+//        System.out.printf("Factorial of %d is %d",num_three,answer);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        sc.close();
+
+
+
+
+
+
+
+
+    }
+}
